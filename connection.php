096967ca7ce13f32b599c3fe956aa8303d8bac4e
Saikat Baul <63208476+saikatbaul@users.noreply.github.com>
2026-05-03 22:50:05 +0600

    Update database connection credentials

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'my_database');
+define('DB_SERVER', 'db.fr-pari1.bengt.wasmernet.com');
+define('DB_USERNAME', '776c21e673208000a7a74e8d6507');
+define('DB_PASSWORD', '069f776c-21e6-745b-8000-6a7a9090100f');
+define('DB_NAME', 'my_database2401');
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
