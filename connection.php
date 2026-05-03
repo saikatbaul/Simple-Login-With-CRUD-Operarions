fe8ab813bf18216b912a68e37f68619ef1bba22c
Saikat Baul <63208476+saikatbaul@users.noreply.github.com>
2026-05-03 23:37:56 +0600

    Update database connection details for local setup

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
-define('DB_SERVER', 'db.fr-pari1.bengt.wasmernet.com');
-define('DB_USERNAME', '776c21e673208000a7a74e8d6507');
-define('DB_PASSWORD', '069f776c-21e6-745b-8000-6a7a9090100f');
-define('DB_NAME', 'my_database2401');
-define('DB_PORT', 10272);
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'my_database');
 
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if($conn === false){
-    die("ERROR: Could not connect (Error #" . mysqli_connect_errno() . "). " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
