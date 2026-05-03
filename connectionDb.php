84481bc13df98bf175f1c1f7fdbb352b4123ea80
Saikat Baul <63208476+saikatbaul@users.noreply.github.com>
2026-05-03 23:30:30 +0600

    Update connectionDb.php

--- connectionDb.php
+++ connectionDb.php
@@ -2,20 +2,23 @@
 function db_conn()
 {
     $servername = "db.fr-pari1.bengt.wasmernet.com";
+    $port = "10272";
     $username = "776c21e673208000a7a74e8d6507";
     $password = "069f776c-21e6-745b-8000-6a7a9090100f";
     $dbname = "my_database2401";
 
     try 
     {
-        $conn = new PDO('mysql:host='.$servername.';dbname='.$dbname.';charset=utf8', $username, $password);
+        $dsn = "mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8";
+        $conn = new PDO($dsn, $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        return $conn;
     } 
     catch (PDOException $e) 
     {
-        echo $e->getMessage();
+        echo "Connection failed: " . $e->getMessage();
+        return null;
     }
-    return $conn;
 }
 ?>
